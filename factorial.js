18879764d3f68e2b6c9c5ab3353f4b93d536b260
Abhinit Rai <abhinit1990@gmail.com>
2025-07-22 21:43:35 +0530

    factorial

---
+++ factorial.js
@@ -0,0 +1,13 @@
+function calculateFactorial(n){
+
+    if(n==1 || n==0){
+        return 1;
+    } else if(n>1){
+        return n * calculateFactorial(n-1)
+    } else {
+        return "Invalid input: n must be a non-negative integer.";
+    }
+}
+
+const res = calculateFactorial(11)
+console.log(res); 
